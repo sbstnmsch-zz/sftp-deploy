a00ffae617563a427b081b1e0705b55d5a06ce8f
Sebastian Misch <sebastian.misch@westernacher.com>
2017-05-05 13:44:40 +0200

    Initial checkin

---
+++ README.md
@@ -0,0 +1,28 @@
+# SFTP Deploy
+Deploy a path to a remote server via SFTP
+
+# Install
+```
+$ npm install -D sftp-deploy
+```
+
+# Setup
+Deploy via npm-script in *package.json*:
+```
+  "scripts": {
+    ...
+    "deploy": "sftp-deploy"
+  }
+```
+
+# Deploy via CI - Continous integration
+In your build environment (i.e. travis-ci, teamcity, bamboo)
+setup the following environment variables:
+```
+DEPLOY_HOST=192.168.1.42
+DEPLOY_USER=deploymentuser
+DEPLOY_PASSWORD=!deploymentpa5swOrd
+DEPLOY_PATH=/srv/httpd/htdocs
+DEPLOY_SRC=./dist
+DEPLOY_PORT=22 # Optional
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+const nodeSftpDeploy = require('node-sftp-deploy');
+
+if (
+  process.env.DEPLOY_HOST &&
+  process.env.DEPLOY_USER &&
+  process.env.DEPLOY_PASSWORD &&
+  process.env.DEPLOY_PATH &&
+  process.env.DEPLOY_SRC
+) {
+  nodeSftpDeploy({
+    host: process.env.DEPLOY_HOST,
+    port: process.env.DEPLOY_PORT || '22',
+    user: process.env.DEPLOY_USER,
+    pass: process.env.DEPLOY_PASSWORD,
+    remotePath: process.env.DEPLOY_PATH,
+    sourcePath: process.env.DEPLOY_SRC || './'
+  }).then(() => {
+    console.log('<U+1F355>  Deployment done.');
+  }).catch((err) => {
+    console.error('Deployment error:', err);
+  });
+} else {
+  console.warn(
+    'Deployment omitted.',
+    'Set DEPLOY_HOST, DEPLOY_USER, DEPLOY_PASSWORD, DEPLOY_PATH to deploy.'
+  );
+}
